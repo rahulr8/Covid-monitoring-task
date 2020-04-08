723c01cdf940f7886fadc14c48f6b2dbfc5264a0
Rahulio <rahuliodev8@gmail.com>
2020-04-08 19:02:59 -0400

    Fetches data from Ontario health API

--- app.py
+++ app.py
@@ -1,18 +1,33 @@
 import firebase_admin
+import requests
+import json
 from firebase_admin import credentials, firestore
 
+# Setup Firestore db
 cred = credentials.Certificate(
     "./serviceAccountKey.json")
-
 default_app = firebase_admin.initialize_app(cred)
-
 db = firestore.client()
 
-quote = "Yep quote"
-author = "Nope author"
+URL = "https://data.ontario.ca/api/3/action/datastore_search?resource_id=455fd63b-603d-4608-8216-7d8647f43350"
+LIMIT = 5
+OFFSET = 0
 
-doc_ref = db.collection(u'sampleData').document(u'inspiration')
-doc_ref.set({
-    u'quote': quote,
-    u'author': author,
-})
+QUERY = f'{URL}&limit={LIMIT}&offset={OFFSET}'
+
+response = requests.get(QUERY).text
+response_data = json.loads(response)
+
+print(json.dumps(response_data, indent=4))
+
+# quote = "Yep quote"
+# author = "Nope author"
+
+# doc_ref = db.collection(u'sampleData').document(u'inspiration')
+# doc_ref.set({
+#     u'quote': quote,
+#     u'author': author,
+# })
+
+# doc = doc_ref.get()
+# print(doc.to_dict())
