4e5e11dfe0fe90bb360984107a4d257ff67745ed
Rahulio <rahuliodev8@gmail.com>
2020-04-10 17:07:16 -0400

    Refactor + Add new metadata

--- loadFirestore.py
+++ loadFirestore.py
@@ -1,34 +1,37 @@
 import firebase_admin
-# import requests
-# import json
-# from firebase_admin import credentials, firestore
+import requests
+import json
+from firebase_admin import credentials, firestore
 
-from writeToDb import writeCovidDataToDb, helloWorld
-
-helloWorld()
+from writeToDb import writeCovidDataToDb
 
 # Setup Firestore db
-# cred = credentials.Certificate("./serviceAccountKey.json")
-# default_app = firebase_admin.initialize_app(cred)
-# db = firestore.client()
+cred = credentials.Certificate("./serviceAccountKey.json")
+default_app = firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 # Fetch data from Ontario Public Health API
-# URL = "https://data.ontario.ca/api/3/action/datastore_search?resource_id=455fd63b-603d-4608-8216-7d8647f43350"
-# LIMIT = 10000  # Should reflect Max number of cases in Ontario
-# OFFSET = 0
-# QUERY = f'{URL}&limit={LIMIT}&offset={OFFSET}'
+URL = "https://data.ontario.ca/api/3/action/datastore_search?resource_id=455fd63b-603d-4608-8216-7d8647f43350"
+LIMIT = 10000  # Should reflect Max number of cases in Ontario
+OFFSET = 0
+QUERY = f'{URL}&limit={LIMIT}&offset={OFFSET}'
 
-# response = requests.get(QUERY).text
-# response_data = json.loads(response)
-# results = response_data.get("result")
+response = requests.get(QUERY).text
+response_data = json.loads(response)
+results = response_data.get("result")
 
-# recordsFromFirestore = results.get("records")
-# TOTAL_RESULTS = results.get("total")
+recordsFromFirestore = results.get("records")
+TOTAL_RESULTS = results.get("total")
 
 
 # Write to db in chunks of 'WRITE_OFFSET' since firestore does not accept large pieces of data
 WRITE_OFFSET = 500
 
-# for limit in range(0, TOTAL_RESULTS, WRITE_OFFSET):
-#     writeCovidDataToDb(limit, WRITE_OFFSET, TOTAL_RESULTS,
-#                        recordsFromFirestore)
+for limit in range(0, TOTAL_RESULTS, WRITE_OFFSET):
+    writeCovidDataToDb(
+        limit,
+        WRITE_OFFSET,
+        TOTAL_RESULTS,
+        recordsFromFirestore,
+        db
+    )
--- writeToDb.py
+++ writeToDb.py
@@ -1,12 +1,41 @@
+# Gender
+gender_data = {
+    "total_male": 0,
+    "total_female": 0,
+}
+
+# Age group
+age_group_data = {
+    "<20": 0,
+    "20s": 0,
+    "30s": 0,
+    "40s": 0,
+    "50s": 0,
+    "60s": 0,
+    "70s": 0,
+    "80s": 0,
+    "90s": 0,
+    "Unknown": 0,
+}
+
+# Acquisition type
+acquisition_type_data = {
+    "community_spread": 0,
+    "travel_related": 0,
+    "neither": 0,
+    "unknown": 0,
+}
+
+
 # Write data to firestore db
-def writeCovidDataToDb(limit, write_offset, total_results, records):
-    data_to_write = []
+def writeCovidDataToDb(limit, write_offset, total_results, records, db):
 
     for record in records[limit:limit+write_offset]:
         id = record["ROW_ID"]
         episode_date = record["ACCURATE_EPISODE_DATE"]
         age_group = record["Age_Group"]
         gender = record["CLIENT_GENDER"]
+        acquisition_info = record["CASE_ACQUISITIONINFO"]
         case_outcome = record["OUTCOME1"]
         reporting_phu_address = record["Reporting_PHU_Address"]
         reporting_phu_city = record["Reporting_PHU_City"]
@@ -14,18 +43,33 @@ def writeCovidDataToDb(limit, write_offset, total_results, records):
         reporting_phu_longitude = record["Reporting_PHU_Longitude"]
         reporting_phu_postal_code = record["Reporting_PHU_Postal_Code"]
 
-        data_to_write.append({
-            "id": id,
-            "episode_date": episode_date,
-            "age_group": age_group,
-            "gender": gender,
-            "case_outcome": case_outcome,
-            "reporting_phu_address": reporting_phu_address,
-            "reporting_phu_city": reporting_phu_city,
-            "reporting_phu_latitude": reporting_phu_latitude,
-            "reporting_phu_longitude": reporting_phu_longitude,
-            "reporting_phu_postal_code": reporting_phu_postal_code,
-        })
+        # Create emergent data here
+        # postGenderData(db, gender)
+        # postAgeGroupData(db, age_group)
+        postacquisitionData(db, acquisition_info)
+
+        # Post emergent data to db
+
+        # Finally post raw data to db
+        # postRawDataToDb(db, limit, write_offset, total_results)
+
+    print(acquisition_type_data)
+
+
+def postRawDataToDb(db, limit, write_offset, total_results):
+    data_to_write = []
+    data_to_write.append({
+        "id": id,
+        "episode_date": episode_date,
+        "age_group": age_group,
+        "gender": gender,
+        "case_outcome": case_outcome,
+        "reporting_phu_address": reporting_phu_address,
+        "reporting_phu_city": reporting_phu_city,
+        "reporting_phu_latitude": reporting_phu_latitude,
+        "reporting_phu_longitude": reporting_phu_longitude,
+        "reporting_phu_postal_code": reporting_phu_postal_code,
+    })
 
     # Write to cloud firestore
     LOWER_LIMIT = limit
@@ -34,10 +78,52 @@ def writeCovidDataToDb(limit, write_offset, total_results, records):
     generic_data_ref = db.collection(
         u'ontarioData').document(f'range-{LOWER_LIMIT}-{UPPER_LIMIT}')
 
-    generic_data_ref.set({
-        "data": data_to_write
-    })
+    # Post to db
+    # generic_data_ref.set({
+    #     "data": data_to_write
+    # })
 
 
-def helloWorld():
-    print("Yes I have been called")
+def postGenderData(db, gender):
+    GENDER_MALE = "MALE"
+    GENDER_FEMALE = "FEMALE"
+
+    if(gender == GENDER_MALE):
+        gender_data["total_male"] += 1
+    elif(gender == GENDER_FEMALE):
+        gender_data["total_female"] += 1
+
+
+def postAgeGroupData(db, age_group):
+
+    if(age_group == "<20"):
+        age_group_data["<20"] += 1
+    elif(age_group == "20s"):
+        age_group_data["20s"] += 1
+    elif(age_group == "30s"):
+        age_group_data["30s"] += 1
+    elif(age_group == "40s"):
+        age_group_data["40s"] += 1
+    elif(age_group == "50s"):
+        age_group_data["50s"] += 1
+    elif(age_group == "60s"):
+        age_group_data["60s"] += 1
+    elif(age_group == "70s"):
+        age_group_data["70s"] += 1
+    elif(age_group == "80s"):
+        age_group_data["80s"] += 1
+    elif(age_group == "90s"):
+        age_group_data["90s"] += 1
+    elif(age_group == "Unknown"):
+        age_group_data["Unknown"] += 1
+
+
+def postacquisitionData(db, acquisition_info):
+    if(acquisition_info == "Contact of a confirmed case"):
+        acquisition_type_data["community_spread"] += 1
+    elif(acquisition_info == "Travel-Related"):
+        acquisition_type_data["travel_related"] += 1
+    elif(acquisition_info == "Information pending"):
+        acquisition_type_data["unknown"] += 1
+    elif(acquisition_info == "Neither"):
+        acquisition_type_data["neither"] += 1
